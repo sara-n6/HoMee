d1579105a0ba667f6ce8cfc5f66dd3cb25f90e72
長岡紗良 <nagaokasara@nagaokasaranonotobukkukonpyuta.local>
2024-12-09 22:25:40 +0900

    ADD:task-index-show

--- rails/app/controllers/api/v1/current/tasks_controller.rb
+++ rails/app/controllers/api/v1/current/tasks_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::Current::TasksController < Api::V1::BaseController
   before_action :authenticate_user!
 
+  def index
+    tasks = current_user.tasks.not_unsaved.order(created_at: :desc)
+    render json: tasks
+  end
+
+  def show
+    task = current_user.tasks.find(params[:id])
+    render json: task
+  end
+
   def create
     unsaved_task = current_user.tasks.unsaved.first || current_user.tasks.create!(status: :unsaved)
     render json: unsaved_task
--- rails/spec/requests/api/v1/current/tasks_spec.rb
+++ rails/spec/requests/api/v1/current/tasks_spec.rb
@@ -1,6 +1,67 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Current::Tasks", type: :request do
+  describe "GET api/v1/current/tasks" do
+    subject { get(api_v1_current_tasks_path, headers:) }
+
+    let(:headers) { current_user.create_new_auth_token }
+    let(:current_user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    before { create_list(:task, 2, user: other_user) }
+
+    context "ログインユーザーに紐づく tasks レコードが存在する時" do
+      before { create_list(:task, 3, user: current_user) }
+
+      it "正常にレコードを取得できる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.length).to eq 3
+        expect(res[0].keys).to eq ["id", "title", "body", "status", "created_at", "from_today", "user"]
+        expect(res[0]["user"].keys).to eq ["name"]
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "ログインユーザーに紐づく tasks レコードが存在しない時" do
+      it "空の配列が返る" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res).to eq []
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "GET api/v1/current/tasks/:id" do
+    subject { get(api_v1_current_task_path(id), headers:) }
+
+    let(:headers) { current_user.create_new_auth_token }
+    let(:current_user) { create(:user) }
+
+    context ":id がログインユーザーに紐づく tasks レコードの id である時" do
+      let(:current_user_task) { create(:task, user: current_user) }
+      let(:id) { current_user_task.id }
+
+      it "正常にレコードを取得できる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.keys).to eq ["id", "title", "body", "status", "created_at", "from_today", "user"]
+        expect(res["user"].keys).to eq ["name"]
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context ":id がログインユーザーに紐づく tasks レコードの id ではない時" do
+      let(:other_user_task) { create(:task) }
+      let(:id) { other_user_task.id }
+
+      it "例外が発生する" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
   describe "POST api/v1/current/tasks" do
     subject { post(api_v1_current_tasks_path, headers:) }
 
