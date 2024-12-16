7642cf2a4606639dfc0575073fac8c26ae37b692
長岡紗良 <nagaokasara@nagaokasaranonotobukkukonpyuta.local>
2024-12-16 22:04:32 +0900

    サインアウト実装

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -1,10 +1,37 @@
-import { AppBar, Box, Button, Container } from '@mui/material'
+import Logout from '@mui/icons-material/Logout'
+import PersonIcon from '@mui/icons-material/Person'
+import TaskIcon from '@mui/icons-material/Task'
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  Typography,
+} from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { useUserState } from '@/hooks/useGlobalState'
 
 const Header = () => {
+  const router = useRouter()
   const [user] = useUserState()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   return (
     <AppBar
@@ -13,7 +40,7 @@ const Header = () => {
         backgroundColor: 'white',
         color: 'black',
         boxShadow: 'none',
-        py: '12px',
+        py: 1,
       }}
     >
       <Container maxWidth="lg" sx={{ px: 2 }}>
@@ -26,34 +53,36 @@ const Header = () => {
         >
           <Box>
             <Link href="/">
-              <Image src="/logohomee.png" width={133} height={40} alt="logo" />
+              <Image src="/logo.png" width={133} height={40} alt="logo" />
             </Link>
           </Box>
           {user.isFetched && (
             <>
               {!user.isSignedIn && (
                 <Box>
-                  <Link href="/sign_in">
-                    <Button
-                      color="primary"
-                      variant="contained"
-                      sx={{
-                        color: 'white',
-                        textTransform: 'none',
-                        fontSize: 16,
-                        borderRadius: 2,
-                        boxShadow: 'none',
-                      }}
-                    >
-                      ログイン
-                    </Button>
-                  </Link>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    sx={{
+                      color: 'white',
+                      textTransform: 'none',
+                      fontSize: 16,
+                      borderRadius: 2,
+                      boxShadow: 'none',
+                    }}
+                    onClick={() => {
+                      router.push('/sign_in')
+                    }}
+                  >
+                    ログイン
+                  </Button>
                   <Button
                     color="primary"
                     variant="outlined"
                     sx={{
                       textTransform: 'none',
                       fontSize: 16,
+                      lineHeight: '27px',
                       borderRadius: 2,
                       boxShadow: 'none',
                       border: '1.5px solid #3EA8FF',
@@ -64,7 +93,59 @@ const Header = () => {
                   </Button>
                 </Box>
               )}
-              {user.isSignedIn && <Box>{user.name}</Box>}
+              {user.isSignedIn && (
+                <Box sx={{ display: 'flex' }}>
+                  <IconButton onClick={handleClick} sx={{ p: 0 }}>
+                    <Avatar>
+                      <PersonIcon />
+                    </Avatar>
+                  </IconButton>
+                  <Box sx={{ ml: 2 }}>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      sx={{
+                        color: 'white',
+                        textTransform: 'none',
+                        fontSize: 16,
+                        borderRadius: 2,
+                        width: 100,
+                        boxShadow: 'none',
+                      }}
+                    >
+                      新規作成
+                    </Button>
+                  </Box>
+                  <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                  >
+                    <Box sx={{ pl: 2, py: 1 }}>
+                      <Typography sx={{ fontWeight: 'bold' }}>
+                        {user.name}
+                      </Typography>
+                    </Box>
+                    <Divider />
+                    <MenuItem>
+                      <ListItemIcon>
+                        <TaskIcon fontSize="small" />
+                      </ListItemIcon>
+                      タスクの管理
+                    </MenuItem>
+                    <Link href="/sign_out">
+                      <MenuItem>
+                        <ListItemIcon>
+                          <Logout fontSize="small" />
+                        </ListItemIcon>
+                        サインアウト
+                      </MenuItem>
+                    </Link>
+                  </Menu>
+                </Box>
+              )}
             </>
           )}
         </Box>
