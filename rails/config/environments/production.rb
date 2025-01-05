3a0af3cfa2da8ec351999d1fbb09daafdbc3a716
長岡紗良 <nagaokasara@nagaokasaranonotobukkukonpyuta.local>
2025-01-05 18:52:03 +0900

    RenderとVercelにデプロイ準備とメール送信準備

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -78,4 +78,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 認証メール送信に関する記述
+  config.action_mailer.default_options = { from: "no-reply@homee-weld.vercel.app" }
+  config.action_mailer.default_url_options = { host: "https://homee-weld.vercel.app" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV["GMAIL_USER_NAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
 end
